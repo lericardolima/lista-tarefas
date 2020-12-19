d70422f576c303880c9fc40dda094be2403eedb3
lericardolima <lericardolima@gmail.com>
2020-12-18 22:55:15 -0300

    Criar tarefa

--- src/controllers/tarefas.js
+++ src/controllers/tarefas.js
@@ -1,4 +1,21 @@
+const Tarefas = require('../models/tarefas');
+
 exports.create = (req, res) => {
+    
+    new Tarefas(req.body)
+        .save((error, tarefa) => {
+            if (error) {
+                return res.status(400).send({
+                    erro: error.name,
+                    mensagem: error.message
+                })
+            }
+
+            return res.status(201).send({
+                mensagem: 'Tarefa criada com sucesso!',
+                tarefa
+            });
+        })
 
 }
 
