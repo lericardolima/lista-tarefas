f977a43b38f8b0752fcc3cdc22734f14a9080b55
lericardolima <lericardolima@gmail.com>
2020-12-18 22:00:56 -0300

    Mapear controllers

---
+++ src/controllers/tarefas.js
@@ -0,0 +1,19 @@
+exports.create = (req, res) => {
+
+}
+
+exports.list = (req, res) => {
+
+}
+
+exports.get = (req, res) => {
+
+}
+
+exports.update = (req, res) => {
+
+}
+
+exports.delete = (req, res) => {
+
+}
